3cf842c7088040823bc8ea78b9e0dc98a38fd1c4
axiu0929 <e2973077401@gmail.com>
2022-03-07 15:49:24 +0800

    send and receive

--- src/main.c
+++ src/main.c
@@ -5,10 +5,12 @@
 #include "driver/gpio.h"
 #include <rom/ets_sys.h>
 
-#define GPIO_OUTPUT_IO   GPIO_NUM_3
-#define GPIO_INPUT_IO    GPIO_NUM_21
-#define GPIO_OUTPUT_TEST GPIO_NUM_27
+#define GPIO_OUTPUT_IO       GPIO_NUM_3
+#define GPIO_OUTPUT_TEST     GPIO_NUM_27
 #define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO) | (1ULL<<GPIO_OUTPUT_TEST))
+
+#define GPIO_INPUT_IO        GPIO_NUM_4
+#define GPIO_INPUT_PIN_SEL   (1ULL<<GPIO_OUTPUT_IO)
 
 void send_byte(char data)
 {
@@ -34,16 +36,15 @@ void send_byte(char data)
 
 char receive_byte(void)
 {
-    
     uint32_t const delay = 1000000 / 9600;
     gpio_set_direction(GPIO_INPUT_IO, GPIO_MODE_INPUT);
+    char data = 0;
     
     while (1)
     {
-        char data = 0;
-
         if (gpio_get_level(GPIO_INPUT_IO) == 0) {
-            ets_delay_us(delay);
+            //gpio_set_level(GPIO_OUTPUT_TEST, 1);
+            ets_delay_us(delay*3/2);
             
             for (int i = 0; i < 8; ++i) 
             {
@@ -52,7 +53,8 @@ char receive_byte(void)
                 data = data | bit_HL;
                 ets_delay_us(delay);
             }
-    
+
+            //gpio_set_level(GPIO_OUTPUT_TEST, 0);
             return data;
         }
     }
@@ -63,24 +65,27 @@ char receive_byte(void)
 
 void app_main(void)
 {
-    //zero-initialize the config structure.
     gpio_config_t io_conf = {};
-    //disable interrupt
     io_conf.intr_type = GPIO_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_OUTPUT;
-    //bit mask of the pins that you want to set,e.g.GPIO18/19
     io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
     io_conf.pull_down_en = 0;
     io_conf.pull_up_en = 0;
-    //configure GPIO with the given settings
     gpio_config(&io_conf);
-   /*
+    
     gpio_set_level(GPIO_OUTPUT_IO, 1);
     vTaskDelay (1);
     
-    send_byte('T');
+    // send   
+    gpio_set_level(GPIO_OUTPUT_TEST, 0);
+    
+    /*
+    for (;;) {
+        send_byte(0x78);
+        //send_byte(receive_byte());
+    } 
     */
-   
+
     /*
     for (int c = 65; c < 91; ++c) {
         send_byte(c);
@@ -89,16 +94,10 @@ void app_main(void)
         send_byte(c);
     } 
     */
-    //char tmp = 0;
-    
-    //receive_byte();
-    gpio_set_level(GPIO_OUTPUT_TEST, 0);
-    gpio_set_level(GPIO_OUTPUT_IO, 1);
-    vTaskDelay (1);
+
+    // receive
+    send_byte('T');
     for (;;) {
-        send_byte(0x78);
-    } 
-    //send_byte('T');
-    //send_byte(receive_byte());      
-    //send_byte('T');
+        send_byte(receive_byte());
+    }
 }
