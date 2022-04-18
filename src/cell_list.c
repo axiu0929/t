36d6e250aff4f2cf8d07cd5e7d1bb7438143d6a9
axiu0929 <e2973077401@gmail.com>
2022-04-18 17:41:32 +0800

    add function make_wifi_list

---
+++ src/cell_list.c
@@ -0,0 +1,44 @@
+/*
+Command: AT+QOPS
+
+Response:
+
+For 2G:
++QOPS: <oper_in_string>,<oper_in_short_string>,<oper_in_number>
+<index>,<RAT,<freq>,<lac>,<ci>,<bsic>,<rxlev>,<c1>,<cba>,<is_gprs_support>
+[...]
+
+OK
+we need: <oper_in_number>,lac,ci,rxlev
+
+For 3G:
++QOPS: <oper_in_string>,<oper_in_short_string>,<oper_in_number>
+<index>,<RAT>,<freq>,<psc>,<lac>,<ci>,<rscp>,<ecno>,<cba>
+[...]
+
+OK
+we need: <oper_in_number>,lac,ci,rscp
+
+For 4G:
++QOPS: <oper_in_string>,<oper_in_short_string>,<oper_in_number>
+<index>,<RAT>,<freq>,<pci>,<tac>,<ci>,<rsrp>,<rsrq>,<cba>
+[...]
+
+OK
+we need: <oper_in_number>,tac,ci,rsrp
+*/
+
+/*
+loop until OK
+
+if +: 讀到第2個, 存<oper_in_number>直到 0x0A or 0x0D
+
+else:
+2G:
+讀到第3個, 存<lac>直到, 存<ci>直到, 再讀到1個, 存<rxlev>直到, 讀到0x0A or 0x0D
+3G:
+讀到第4個, 存<lac>直到, 存<ci>直到, 存<rscp>直到, 讀到0x0A or 0x0D
+4G:
+讀到第4個, 存<tac>直到, 存<ci>直到, 存<rsrp>直到, 讀到0x0A or 0x0D
+*/
+
--- src/main.c
+++ src/main.c
@@ -23,16 +23,16 @@ typedef struct ap_info
     int8_t rssi;            // signal strength of AP
 }AP_info;
 
-
 static const char *TAG = "scan";
 
 /* Initialize Wi-Fi as sta and set scan method */
-static uint16_t wifi_scan(AP_info *AP_list)
+uint16_t wifi_scan(AP_info *AP_list)
 {
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
     assert(sta_netif);
+    
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
@@ -61,7 +61,7 @@ static uint16_t wifi_scan(AP_info *AP_list)
     return ap_count;
 }
 
-void app_main(void)
+void make_wifi_list(void)
 {
     // Initialize NVS
     esp_err_t ret = nvs_flash_init();
@@ -98,6 +98,10 @@ void app_main(void)
     }
     for (int i = 0; i < AP_count ; i++) {
         ESP_LOGI("str", "%s", AP_list_str[i]);
-    }
-    
+    }  
+}
+
+void app_main(void)
+{
+    make_wifi_list();
 }
